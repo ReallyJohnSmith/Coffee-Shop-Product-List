6f339e7fa6ba17c4538e8ba51b1515e16feb26a7
ReallyJohnSmith <reallyjohnsmith21@gmail.com>
2022-07-27 16:45:43 -0400

    Add project files.

---
+++ Coffee Shop Product List/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Coffee_Shop_Product_List.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Coffee_Shop_Product_List.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly CoffeeShopProductContext _CoffeeShopProductContext;
+
+        public HomeController(ILogger<HomeController> logger, CoffeeShopProductContext newCoffeeShopProductContext)
+        {
+            _logger = logger;
+            _CoffeeShopProductContext = newCoffeeShopProductContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayMyProducts()
+        {
+            var products = _CoffeeShopProductContext.Products.ToArray();
+            return View(products);
+        }
+
+        public IActionResult MyProductDetails(int ID)
+        {
+            Products foundProduct = null;
+            foreach (var currProduct in _CoffeeShopProductContext.Products.ToArray())
+            {
+                if (currProduct.ID == ID)
+                {
+                    foundProduct = currProduct;
+                    break;
+                }
+            }
+            return View(foundProduct);
+        }
+
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Coffee Shop Product List/Program.cs
@@ -0,0 +1,94 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel.DataAnnotations;
+
+
+
+namespace Coffee_Shop_Product_List
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            // builder.Services.AddControllersWithViews();
+
+            var startup = new Startup(builder.Configuration);
+            startup.ConfigureServices(builder.Services);
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+
+    public class Startup
+    {
+        public Startup(IConfigurationRoot configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfigurationRoot Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<CoffeeShopProductContext>(options =>
+                            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(x => x.MapControllers());
+        }
+    }
+
+    public class CoffeeShopProductContext : DbContext
+    {
+        public CoffeeShopProductContext(DbContextOptions<CoffeeShopProductContext> options) : base(options)
+        {
+            // Nothing needed
+        }
+
+        public DbSet<Products> Products { get; set; }
+    }
+
+    public class Products
+    {
+        [Key]
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal price { get; set; }
+
+        public string category { get; set; }
+    }
+
+
+
+
+}
